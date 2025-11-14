4bd5d30c2647569756e4fe2a9cce6e02b1c2e9a2
yutzu0314 <85773787+yutzu0314@users.noreply.github.com>
2025-11-14 16:59:47 +0800

    11/14 update

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // MySQL
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
+    port: Number(process.env.DB_PORT || 3310),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
